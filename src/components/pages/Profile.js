247cb752bb3dd2f63832bd874eb04b61aac94cfa
Ayush Chauhan <79743704+ayushdev351@users.noreply.github.com>
2021-12-19 23:34:22 +0530

    profile card style complete

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -4,15 +4,18 @@ import "../pages.css";
 import "./Profile.css";
 
 export default function Profile() {
-  const [name, setName] = useState("");
-  const [userName, setUserName] = useState("");
-  const [followers, setFollowers] = useState("");
-  const [following, setFollowing] = useState("");
-  const [repos, setRepos] = useState("");
-  const [avatar, setAvatar] = useState("");
-  const [location, setLocation] = useState("");
-  const [bio, setBio] = useState("");
-  const [userInput, setUserInput] = useState("");
+  const [name, setName] = useState("John Singh");
+  const [userName, setUserName] = useState("John1245");
+  const [followers, setFollowers] = useState("24");
+  const [following, setFollowing] = useState("23");
+  const [repos, setRepos] = useState("5");
+  const [avatar, setAvatar] = useState(
+    "https://avatars.githubusercontent.com/u/15178905?v=4"
+  );
+  const [location, setLocation] = useState("London");
+  const [bio, setBio] = useState("React Developer");
+  const [hireable, setHireable] = useState(false);
+  const [userInput, setUserInput] = useState();
 
   const setData = ({
     name,
@@ -22,7 +25,8 @@ export default function Profile() {
     public_repos,
     avatar_url,
     location,
-    bio
+    bio,
+    hireable
   }) => {
     setName(name);
     setUserName(login);
@@ -32,6 +36,7 @@ export default function Profile() {
     setAvatar(avatar_url);
     setLocation(location);
     setBio(bio);
+    setHireable(hireable);
   };
 
   const changeHandler = (e) => {
@@ -48,6 +53,8 @@ export default function Profile() {
       });
     setUserInput("");
   };
+
+  var status = hireable ? "yes" : "no";
 
   return (
     <div className="profile">
@@ -68,13 +75,18 @@ export default function Profile() {
       <div className="profile-card">
         <img src={avatar} wrapped alt="user" className="profile-img" />
         <br />
-        <h3>{name}</h3>
-        <p>{bio}</p>
-        <p>{userName}</p>
-        <p>{followers}</p>
-        <p>{following}</p>
-        <p>{repos}</p>
-        <p>{location}</p>
+        <div className="card-head">
+          <h2>{name}</h2>
+          <h4>{bio}</h4>
+          <h4 className="user-location">{location}</h4>
+        </div>
+        <div className="card-content">
+          <p>User Name : {userName}</p>
+          <p>Followers : {followers}</p>
+          <p>Following : {following}</p>
+          <p>User repo : {repos}</p>
+          <p>Hireable : {status}</p>
+        </div>
       </div>
     </div>
   );
