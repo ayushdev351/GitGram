b6db7916d4c32c2a6d98b3a40ec9202a15e92cce
Ayush Chauhan <79743704+ayushdev351@users.noreply.github.com>
2021-12-20 00:50:42 +0530

    add location search Feature : Show github users in entered location

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../App.css";
 import "../pages.css";
+import "./Home.css";
 
 export default function Home() {
+  const [users, setUsers] = useState([]);
+  const [userInput, setUserInput] = useState("");
+
+  const setData = ({ items }) => {
+    setUsers(items);
+  };
+
+  const inputHandler = (e) => {
+    e.preventDefault();
+    setUserInput(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    fetch(`https://api.github.com/search/users?q=location:${userInput}`)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  };
+
   return (
-    <>
-      <h1 className="home">Home</h1>
-    </>
+    <div className="home">
+      <form className="form" onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="Enter Location Here"
+          className="user-input"
+          onChange={inputHandler}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="userList">
+        <ul>
+          {users.map((user) => (
+            <div key={user.id}>
+              <p>{user.login}</p>
+              <br />
+              <br />
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
