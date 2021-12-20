2d9666d816c95f7f613ec44b4a23d0444f038b2e
Ayush Chauhan <79743704+ayushdev351@users.noreply.github.com>
2021-12-21 00:20:16 +0530

    complete home and profile

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -3,17 +3,58 @@ import "../../App.css";
 import "../pages.css";
 import "./Profile.css";
 
-import ProfileReuse from "./ProfileReuse";
-
 export default function Profile() {
+  const [name, setName] = useState("John Singh");
+  const [userName, setUserName] = useState("John1245");
+  const [followers, setFollowers] = useState("24");
+  const [following, setFollowing] = useState("23");
+  const [repos, setRepos] = useState("5");
+  const [avatar, setAvatar] = useState(
+    "https://avatars.githubusercontent.com/u/15178905?v=4"
+  );
+  const [location, setLocation] = useState("London");
+  const [bio, setBio] = useState("React Developer");
+  const [hireable, setHireable] = useState(false);
   const [userInput, setUserInput] = useState("");
+
+  const setData = ({
+    name,
+    login,
+    followers,
+    following,
+    public_repos,
+    avatar_url,
+    location,
+    bio,
+    hireable
+  }) => {
+    setName(name);
+    setUserName(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    setAvatar(avatar_url);
+    setLocation(location);
+    setBio(bio);
+    setHireable(hireable);
+  };
 
   const changeHandler = (e) => {
     e.preventDefault();
     setUserInput(e.target.value);
   };
 
-  const submitHandler = () => {};
+  const submitHandler = (e) => {
+    e.preventDefault();
+    fetch(`https://api.github.com/users/${userInput}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+    setUserInput("");
+  };
+
+  var status = hireable ? "yes" : "no";
 
   return (
     <div className="profile">
@@ -31,7 +72,24 @@ export default function Profile() {
           Search
         </button>
       </form>
-      <ProfileReuse fetchedusername={userInput} />
+      <div className="profile">
+      <div className="profile-card">
+        <img src={avatar} wrapped alt="user" className="profile-img" />
+        <br />
+        <div className="card-head">
+          <h2>{name}</h2>
+          <h4 className="bio">{bio}</h4>
+          <h4 className="user-location">{location}</h4>
+        </div>
+        <div className="card-content">
+          <p>User Name : {userName}</p>
+          <p>Followers : {followers}</p>
+          <p>Following : {following}</p>
+          <p>User repo : {repos}</p>
+          <p>Hireable : {status}</p>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
